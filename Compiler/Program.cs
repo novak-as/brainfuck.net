799ffaa2ac4c86a61bb58691156a097af250195b
Oleksandr Novak <novak.as@i.ua>
2016-10-07 21:17:08 +0300

    +->., operations

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Antlr4.Runtime;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,22 +13,55 @@ namespace Compiler
     {
         static void Main(string[] args)
         {
-            var input = ".++.>.++.-.";
+            string filename = args[0];
 
-            Console.WriteLine(input);
+            var text = File.ReadAllText(filename);
 
-            AntlrInputStream inputStream = new AntlrInputStream(input);
+            Console.WriteLine("Compiling...");
+
+            AntlrInputStream inputStream = new AntlrInputStream(text);
             BrainfuckLexer lexer = new BrainfuckLexer(inputStream);
             CommonTokenStream tokenStream = new CommonTokenStream(lexer);
             BrainfuckParser parser = new BrainfuckParser(tokenStream);
 
-            //            var tree = new ParseTreeWalker();
+            var bfVisitor = new Visitor();
+            parser.AddParseListener(bfVisitor);
 
-            //parser.AddParseListener(new Visitor());
+            var lines = new StringBuilder();
+            lines.AppendLine(".assembly extern mscorlib {}");
+            lines.AppendLine(".assembly Test");
+            lines.AppendLine("{");
+            lines.AppendLine(".ver 1:0:1:0");
+            lines.AppendLine("}");
+            lines.AppendLine(".module test.exe");
+            lines.AppendLine(".method static void main() cil managed");
+            lines.AppendLine("{");
+            lines.AppendLine(".maxstack 8");
+            lines.AppendLine(".entrypoint");
+            lines.AppendLine(".locals init ([0] int32[] memory,");
+            lines.AppendLine("[1] int32 currentPosition)");
+            
+            lines.AppendLine("ldc.i4.s 100");
+            lines.AppendLine("newarr [mscorlib]System.Int32");
+            lines.AppendLine("stloc.0");
+            lines.AppendLine("ldc.i4.0");
+            lines.AppendLine("stloc.1");
 
             parser.analyze();
 
-            Console.ReadKey();
+            lines.AppendLine(bfVisitor.Result.ToString());
+
+            lines.AppendLine("ret");
+            lines.Append("}");
+
+            File.WriteAllText("test.il", lines.ToString());
+
+            ProcessStartInfo proc = new System.Diagnostics.ProcessStartInfo();
+            proc.FileName = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\ilasm.exe";
+            proc.Arguments = @"D:\Projects\Brainfuck.net\Compiler\bin\Debug\test.il";
+            Process.Start(proc);
+
+            Console.WriteLine("Done");
         }
     }
 }
--- Compiler/Visitor.cs
+++ Compiler/Visitor.cs
@@ -5,53 +5,109 @@ using System.Text;
 using System.Threading.Tasks;
 using Antlr4.Runtime.Misc;
 
-namespace BFCore
+namespace Compiler
 {
     public class Visitor: BrainfuckBaseListener
     {
-
-        int[] memory = new int[30000];
-        int current = 0;
+        public StringBuilder Result { get; set; }
 
         public Visitor()
         {
+            Result = new StringBuilder();
+        }
 
+        public override void EnterExpr([NotNull] BrainfuckParser.ExprContext context)
+        {
+            base.EnterExpr(context);
         }
 
         public override void EnterRead([NotNull] BrainfuckParser.ReadContext context)
         {
-            memory[current] = Console.Read();
+            
+            A("ldloc.0");
+            A("ldloc.1");
+            A("call int32 [mscorlib]System.Console::Read()");
+            A("stelem.i4");
+            
             base.EnterRead(context);
+        }
+
+        public override void EnterPrint([NotNull] BrainfuckParser.PrintContext context)
+        {
+
+            A("ldstr \"[\"");
+            A("call void [mscorlib] System.Console::Write(string)");
+            A("ldloc.1");
+            A("call void [mscorlib] System.Console::Write(int32)");
+            A("ldstr \"] \"");
+            A("call void [mscorlib] System.Console::Write(string)");
+            A("ldloc.0");
+            A("ldloc.1");
+            A("ldelem.i4");
+            Result.AppendLine("call void [mscorlib] System.Console::WriteLine(int32)");
+
+            /*
+            A("ldloc.0");
+            A("ldloc.1");
+            A("ldelem.i4");
+            Result.AppendLine("call void [mscorlib] System.Console::Write(int32)");
+            */
+
+            base.EnterPrint(context);
         }
 
         public override void EnterAdd([NotNull] BrainfuckParser.AddContext context)
         {
-            memory[current] += 1;
+            A("ldloc.0");
+            A("ldloc.1");
+            A("ldloc.0");
+            A("ldloc.1");
+            A("ldelem.i4");
+            A("ldc.i4.1");
+            A("add");
+            A("stelem.i4");
             base.EnterAdd(context);
         }
 
         public override void EnterNext([NotNull] BrainfuckParser.NextContext context)
         {
-            current += 1;
+            A("ldloc.1");
+            A("ldc.i4.1");
+            A("add");
+            A("stloc.1");
             base.EnterNext(context);
         }
 
         public override void EnterPrev([NotNull] BrainfuckParser.PrevContext context)
         {
-            current -= 1;
+            A("ldloc.1");
+            A("ldc.i4.1");
+            A("sub");
+            A("stloc.1");
             base.EnterPrev(context);
         }
 
         public override void EnterSub([NotNull] BrainfuckParser.SubContext context)
         {
-            memory[current] -= 1;
+            A("ldloc.0");
+            A("ldloc.1");
+            A("ldloc.0");
+            A("ldloc.1");
+            A("ldelem.i4");
+            A("ldc.i4.1");
+            A("sub");
+            A("stelem.i4");
             base.EnterSub(context);
         }
 
-        public override void EnterPrint([NotNull] BrainfuckParser.PrintContext context)
+        public override void EnterLoop([NotNull] BrainfuckParser.LoopContext context)
         {
-            Console.Write(memory[current]);
-            base.EnterPrint(context);
+            base.EnterLoop(context);
+        }
+
+        public override void ExitLoop([NotNull] BrainfuckParser.LoopContext context)
+        {
+            base.ExitLoop(context);
         }
 
         public override void EnterEloop([NotNull] BrainfuckParser.EloopContext context)
@@ -62,6 +118,11 @@ namespace BFCore
         public override void EnterSloop([NotNull] BrainfuckParser.SloopContext context)
         {
             base.EnterSloop(context);
+        }
+
+        private void A(String command)
+        {
+            Result.AppendLine(command);
         }
     }
 }
