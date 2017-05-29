3a004cafd33b2ef2ce08e21f4229caeb7c0faabb
Oleksandr Novak <novak.as@i.ua>
2017-05-29 10:03:03 +0300

    remove deprecated grammar fron anlr build

--- BFCore/_build/antlrBuild.bat
+++ BFCore/_build/antlrBuild.bat
@@ -1,6 +1,5 @@
 cd ..
 
-java -jar _external/antlr-4.5.3-complete.jar -Dlanguage=CSharp -o antlr/Brainfuck grammars/Brainfuck.g4
 java -jar _external/antlr-4.5.3-complete.jar -Dlanguage=CSharp -o antlr/BrainfuckOptimized grammars/BrainfuckOptimized.g4
 
 cd _build
